7500d2fc359ccfca3017c5bf222647079d843560
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-02-28 13:27:22 +0600

    updates

--- lib/doctor/ui/pages/draweritem/ContactUs.dart
+++ lib/doctor/ui/pages/draweritem/ContactUs.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pro_health/ui/utilities/Constant.dart';
+import 'package:pro_health/doctor/ui/utilities/Constant.dart';
 
 class ContactUs extends StatefulWidget {
   ContactUs({Key key, this.title}) : super(key: key);
