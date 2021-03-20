c39d6b286d035fcf1a2bf716b5b71bc0d547d27e
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-03-20 18:14:07 +0600

    updates

--- lib/doctor/views/auth/signup/terms_and_conditions.dart
+++ lib/doctor/views/auth/signup/terms_and_conditions.dart
@@ -12,11 +12,8 @@ class TermsAndConditions extends StatefulWidget {
 }
 
 class TermsAndConditionsState extends State<TermsAndConditions> {
-
-
   @override
   Widget build(BuildContext context) {
-
     final newAccountPasswordLogo = Container(
       padding: EdgeInsets.only(top: 20),
       child: Hero(
@@ -32,8 +29,15 @@ class TermsAndConditionsState extends State<TermsAndConditions> {
     final forgotPasswordTitle = Container(
       //height: 35,
       padding: EdgeInsets.only(bottom: 5),
-      child: Text('Terms and Conditions',
-        style: TextStyle(fontFamily: 'Segoe', color: kTextLightColor, letterSpacing: 0.5, fontSize: 20, fontWeight: FontWeight.w600), textAlign: TextAlign.center,
+      child: Text(
+        'Terms and Conditions',
+        style: TextStyle(
+            fontFamily: 'Segoe',
+            color: kTextLightColor,
+            letterSpacing: 0.5,
+            fontSize: 20,
+            fontWeight: FontWeight.w600),
+        textAlign: TextAlign.center,
       ),
     );
 
@@ -50,16 +54,22 @@ class TermsAndConditionsState extends State<TermsAndConditions> {
     final forgotPassInstructions = Expanded(
       child: Container(
         height: 450,
-        padding: EdgeInsets.only(left: 25.0, top: 30.0, right: 25.0, bottom: 20.0),
+        padding:
+            EdgeInsets.only(left: 25.0, top: 30.0, right: 25.0, bottom: 20.0),
         child: Text(
           'These terms and conditions are a set of rules about use of an application. '
-              'They set out how users may use the site or application, and what they can and cannot do on the application. '
-              'For example, if a user posts offensive or defamatory content on a website, the owner of the app will want'
-              ' to have terms and conditions to fall back on which clearly state that the owner of the app does not permit '
-              'or take responsibility for that offensive content, and that any liability (such as a defamation claim) '
-              'should therefore sit with the user.\nIn addition, the owner of the site may want to have the ability to '
-              'terminate the user\'s account - and this also will need to be explained in the terms and conditions.', textAlign: TextAlign.justify,
-          style: TextStyle(fontFamily: 'Segoe', fontSize: 19.0, color: kBodyTextColor, fontWeight: FontWeight.w500),
+          'They set out how users may use the site or application, and what they can and cannot do on the application. '
+          'For example, if a user posts offensive or defamatory content on a website, the owner of the app will want'
+          ' to have terms and conditions to fall back on which clearly state that the owner of the app does not permit '
+          'or take responsibility for that offensive content, and that any liability (such as a defamation claim) '
+          'should therefore sit with the user.\nIn addition, the owner of the site may want to have the ability to '
+          'terminate the user\'s account - and this also will need to be explained in the terms and conditions.',
+          textAlign: TextAlign.justify,
+          style: TextStyle(
+              fontFamily: 'Segoe',
+              fontSize: 19.0,
+              color: kBodyTextColor,
+              fontWeight: FontWeight.w500),
         ),
       ),
     );
@@ -68,22 +78,28 @@ class TermsAndConditionsState extends State<TermsAndConditions> {
       child: Row(
         children: [
           Container(
-            width: 205,
+            width: 195,
             padding: EdgeInsets.symmetric(horizontal: 30.0),
             child: MaterialButton(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(24),
               ),
               onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (_) => SignInDoctor()));
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) => SignInDoctor()));
               },
               padding: EdgeInsets.only(top: 4.0, bottom: 5.0),
               color: Colors.red,
-              child: Text('Decline', style: TextStyle(fontFamily: "Segoe", letterSpacing: 0.5, fontSize: 18, color: kWhiteShadow)),
+              child: Text('Decline',
+                  style: TextStyle(
+                      fontFamily: "Segoe",
+                      letterSpacing: 0.5,
+                      fontSize: 18,
+                      color: kWhiteShadow)),
             ),
           ),
           Container(
-            width: 205,
+            width: 195,
             padding: EdgeInsets.symmetric(horizontal: 30.0),
             child: MaterialButton(
               shape: RoundedRectangleBorder(
@@ -94,7 +110,12 @@ class TermsAndConditionsState extends State<TermsAndConditions> {
               },
               padding: EdgeInsets.only(top: 4.0, bottom: 5.0),
               color: kButtonColor,
-              child: Text('Accept', style: TextStyle(fontFamily: "Segoe", letterSpacing: 0.5, fontSize: 18, color: kWhiteShadow)),
+              child: Text('Accept',
+                  style: TextStyle(
+                      fontFamily: "Segoe",
+                      letterSpacing: 0.5,
+                      fontSize: 18,
+                      color: kWhiteShadow)),
             ),
           ),
         ],
