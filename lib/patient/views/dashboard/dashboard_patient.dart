b5aa6c9b27ac8774ad7aca5a53c7824a25c7650d
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-03-25 19:12:41 +0600

    updates

--- lib/patient/views/dashboard/dashboard_patient.dart
+++ lib/patient/views/dashboard/dashboard_patient.dart
@@ -4,9 +4,8 @@ import 'package:pro_health/base/utils/constants.dart';
 import 'package:pro_health/patient/views/bottombar/appointment/appointment_patient.dart';
 import 'package:pro_health/patient/views/bottombar/drug_info/drug_index_patient.dart';
 import 'package:pro_health/patient/views/bottombar/home/home_patient.dart';
-import 'package:pro_health/doctor/views/bottombar/profile/profile_doctor.dart';
 import 'package:pro_health/patient/views/bottombar/message/message_patient.dart';
-
+import 'package:pro_health/patient/views/bottombar/profile/profile_patient.dart';
 
 class DashboardPatient extends StatefulWidget {
   static String tag = 'DashboardPatient';
@@ -17,12 +16,16 @@ class DashboardPatient extends StatefulWidget {
 
 class DashboardPatientState extends State<DashboardPatient> {
   int selectedIndex = 2;
-  List<Widget> listWidgets = [DrugIndexPatient(), AppointmentPatient(), HomePatient(), MessagePatient(), ProfileDoctor()];
-
+  List<Widget> listWidgets = [
+    DrugIndexPatient(),
+    AppointmentPatient(),
+    HomePatient(),
+    MessagePatient(),
+    ProfilePatient()
+  ];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
         backgroundColor: kBackgroundColor,
         body: listWidgets[selectedIndex],
@@ -34,19 +37,59 @@ class DashboardPatientState extends State<DashboardPatient> {
           curveSize: 80,
           style: TabStyle.reactCircle,
           items: [
-            TabItem(icon: Image.asset('assets/icons/patient/druginfo.png', scale: 12.0,), activeIcon: Image.asset('assets/icons/patient/druginfo_active.png', scale: 12.0,)),
-            TabItem(icon: Image.asset('assets/icons/patient/appointment.png', scale: 12.0,), activeIcon: Image.asset('assets/icons/patient/appointment_active.png', scale: 12.0,)),
-            TabItem(icon: Image.asset('assets/icons/patient/home.png', scale: 12.0,), activeIcon: Image.asset('assets/icons/patient/home_active.png', scale: 12.0,)),
-            TabItem(icon: Image.asset('assets/icons/patient/message.png', scale: 12.0,), activeIcon: Image.asset('assets/icons/patient/message_active.png', scale: 12.0,)),
-            TabItem(icon: Image.asset('assets/icons/patient/profile.png', scale: 12.0,), activeIcon: Image.asset('assets/icons/patient/profile_active.png', scale: 12.0,)),
+            TabItem(
+                icon: Image.asset(
+                  'assets/icons/patient/druginfo.png',
+                  scale: 12.0,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icons/patient/druginfo_active.png',
+                  scale: 12.0,
+                )),
+            TabItem(
+                icon: Image.asset(
+                  'assets/icons/patient/appointment.png',
+                  scale: 12.0,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icons/patient/appointment_active.png',
+                  scale: 12.0,
+                )),
+            TabItem(
+                icon: Image.asset(
+                  'assets/icons/patient/home.png',
+                  scale: 12.0,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icons/patient/home_active.png',
+                  scale: 12.0,
+                )),
+            TabItem(
+                icon: Image.asset(
+                  'assets/icons/patient/message.png',
+                  scale: 12.0,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icons/patient/message_active.png',
+                  scale: 12.0,
+                )),
+            TabItem(
+                icon: Image.asset(
+                  'assets/icons/patient/profile.png',
+                  scale: 12.0,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icons/patient/profile_active.png',
+                  scale: 12.0,
+                )),
           ],
           initialActiveIndex: selectedIndex,
           activeColor: kBaseColor,
           onTap: onItemTapped,
-        )
-    );
+        ));
   }
-  void onItemTapped(int index){
+
+  void onItemTapped(int index) {
     setState(() {
       selectedIndex = index;
     });
