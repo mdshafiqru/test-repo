7a5befa3fb14038df4fd4d89f4d824937a6b4442
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-03-30 21:15:33 +0600

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pro_health/app.dart';
+import 'package:pro_health/pro_health.dart';
 
 void main() {
-  runApp(App());
+  runApp(ProHealth());
 }
