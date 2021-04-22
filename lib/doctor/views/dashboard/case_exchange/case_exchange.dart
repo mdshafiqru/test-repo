490a076009d3d3f1eb00c73bbcadfadc05003197
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-04-22 18:42:03 +0600

    updates

--- lib/doctor/views/dashboard/case_exchange/case_exchange.dart
+++ lib/doctor/views/dashboard/case_exchange/case_exchange.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:pro_health/base/utils/constants.dart';
+import 'package:pro_health/doctor/views/dashboard/case_exchange/create_case.dart';
+import 'package:pro_health/doctor/views/dashboard/case_exchange/saved_case.dart';
+import 'package:pro_health/doctor/views/dashboard/case_exchange/case_details.dart';
 
 class CaseExchange extends StatefulWidget {
   static String tag = 'CaseExchange';
@@ -9,6 +13,7 @@ class CaseExchange extends StatefulWidget {
 }
 
 class CaseExchangeState extends State<CaseExchange> {
+  bool hasBeenPressed = false;
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
@@ -30,24 +35,45 @@ class CaseExchangeState extends State<CaseExchange> {
         ),
         SizedBox(
           height: 50,
-          width: 35,
-          child: Container(
+          width: 50,
+          child: PopupMenuButton(
             padding: EdgeInsets.only(left: 0.0, right: 10.0),
-            child: IconButton(
-              padding: EdgeInsets.zero,
-              icon: Icon(Icons.more_vert_rounded),
-              color: kTitleColor,
-              onPressed: () {},
+            child: Container(
+              padding: EdgeInsets.all(2),
+              child: Icon(
+                Icons.more_vert_rounded,
+                color: kTitleColor,
+              ),
             ),
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem<String>(
+                  value: 'SavedCase',
+                  height: 30,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 25),
+                    child: Text('Saved Case'),
+                  ),
+                ),
+              ];
+            },
+            onSelected: (result) {
+              if (result == 'SavedCase') {
+                Navigator.of(context).pushNamed(SavedCase.tag);
+              }
+            },
           ),
         ),
       ],
     );
     final writeNewCase = Container(
       height: 60,
+      width: 410,
       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
       child: MaterialButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).pushNamed(CreateCase.tag);
+        },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(25),
         ),
@@ -64,46 +90,43 @@ class CaseExchangeState extends State<CaseExchange> {
     );
     final categoryTotalLabel = Container(
       padding: EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 10),
-      child: Column(
+      child: Row(
         children: [
-          Row(
-            children: [
-              Container(
-                width: 200,
-                height: 35,
-                padding: EdgeInsets.only(left: 5),
-                child: DropdownButtonFormField(
-                  decoration: new InputDecoration(
-                    hintText: "All",
-                    contentPadding: EdgeInsets.fromLTRB(15.0, 2.0, 5.0, 0.0),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(32.0)),
-                  ),
-                  items: <DropdownMenuItem>[
-                    DropdownMenuItem<int>(
-                      value: 1,
-                      child: Text("All"),
-                    ),
-                    DropdownMenuItem<int>(
-                      value: 2,
-                      child: Text("Pediatrician"),
-                    ),
-                  ],
-                  onChanged: (val) => print(val),
-                  onSaved: (val) => print(val),
-                ),
+          Container(
+            width: 200,
+            height: 35,
+            padding: EdgeInsets.only(left: 5),
+            child: DropdownButtonFormField(
+              decoration: new InputDecoration(
+                hintText: "All",
+                contentPadding: EdgeInsets.fromLTRB(15.0, 2.0, 5.0, 0.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
               ),
-              Container(
-                padding: EdgeInsets.only(left: 10, right: 5),
-                child: Text(
-                  '(1,000) case found',
-                  style: TextStyle(
-                      fontFamily: 'Segoe',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600),
+              items: <DropdownMenuItem>[
+                DropdownMenuItem<int>(
+                  value: 1,
+                  child: Text("All"),
                 ),
-              ),
-            ],
+                DropdownMenuItem<int>(
+                  value: 2,
+                  child: Text("Pediatrician"),
+                ),
+              ],
+              onChanged: (val) => print(val),
+              onSaved: (val) => print(val),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 10, right: 5),
+            child: Text(
+              '(1,000) case found',
+              style: TextStyle(
+                  fontFamily: 'Segoe',
+                  fontSize: 15,
+                  color: kTextLightColor,
+                  fontWeight: FontWeight.w600),
+            ),
           ),
         ],
       ),
@@ -118,24 +141,24 @@ class CaseExchangeState extends State<CaseExchange> {
       ),
     );
     final doctorCaseCard = Container(
-      padding: EdgeInsets.only(left: 8, top: 10, right: 8, bottom: 10),
+      padding: EdgeInsets.only(left: 8, top: 2, right: 8, bottom: 2),
       child: Card(
         margin: EdgeInsets.fromLTRB(8.0, 10.0, 8.0, 0.0),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
+          borderRadius: BorderRadius.circular(20),
         ),
         elevation: 5,
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(15.0),
+          borderRadius: BorderRadius.circular(20.0),
           child: Column(
             children: [
               Container(
-                height: 90,
+                height: 80,
                 color: kCardTitleColor,
                 child: Column(
                   children: [
                     Container(
-                      height: 60,
+                      height: 50,
                       child: Row(
                         children: [
                           Container(
@@ -156,11 +179,11 @@ class CaseExchangeState extends State<CaseExchange> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Container(
-                                  padding: EdgeInsets.only(top: 15),
+                                  padding: EdgeInsets.only(top: 10),
                                   child: Text(
                                     'Prof. Mohammed Hanif',
                                     style: TextStyle(
-                                        fontSize: 16,
+                                        fontSize: 14,
                                         fontFamily: 'Segoe',
                                         fontWeight: FontWeight.bold,
                                         color: kBaseColor),
@@ -170,7 +193,7 @@ class CaseExchangeState extends State<CaseExchange> {
                                   child: Text(
                                     'MBBS, FCPS, FRCP (Edin)',
                                     style: TextStyle(
-                                      fontSize: 13,
+                                      fontSize: 12,
                                       fontWeight: FontWeight.w600,
                                       fontFamily: 'Segoe',
                                     ),
@@ -179,55 +202,76 @@ class CaseExchangeState extends State<CaseExchange> {
                               ],
                             ),
                           ),
-                          SizedBox(width: 115),
+                          SizedBox(width: 135),
                           Container(
                             width: 30,
                             height: 60,
                             padding: EdgeInsets.only(top: 15),
-                            child: IconButton(
+                            child: PopupMenuButton(
                               padding: EdgeInsets.zero,
                               icon: Icon(
                                 Icons.more_vert_rounded,
                                 size: 28,
                               ),
-                              onPressed: () {},
+                              itemBuilder: (BuildContext context) {
+                                return [
+                                  PopupMenuItem<String>(
+                                    value: 'Save',
+                                    height: 30,
+                                    mouseCursor: MouseCursor.defer,
+                                    child: Container(
+                                      padding: EdgeInsets.only(left: 20),
+                                      child: Text('Save'),
+                                    ),
+                                  ),
+                                ];
+                              },
+                              onSelected: (result) {
+                                if (result == 'Save') {
+                                  Navigator.of(context)
+                                      .pushNamed(SavedCase.tag);
+                                }
+                              },
                             ),
                           ),
                         ],
                       ),
                     ),
                     Container(
-                      height: 30,
+                      height: 25,
                       padding: EdgeInsets.only(bottom: 3),
                       child: Row(
                         children: [
                           Container(
                             width: 140,
+                            padding: EdgeInsets.only(left: 30),
                             child: Row(
                               children: [
-                                IconButton(
-                                  padding: EdgeInsets.zero,
-                                  icon: Icon(
-                                    Icons.category_outlined,
-                                    color: kButtonColor,
-                                    size: 18,
+                                Container(
+                                  width: 15,
+                                  height: 25,
+                                  child: Image.asset(
+                                    'assets/icons/doctor/category.png',
+                                    fit: BoxFit.contain,
                                   ),
-                                  onPressed: () {},
                                 ),
-                                Text(
-                                  'Pediatrician',
-                                  style: TextStyle(
-                                    fontFamily: 'Segoe',
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.green,
+                                Container(
+                                  padding: EdgeInsets.only(left: 4),
+                                  child: Text(
+                                    'Pediatrician',
+                                    style: TextStyle(
+                                      fontFamily: 'Segoe',
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.green,
+                                    ),
                                   ),
                                 ),
                               ],
                             ),
                           ),
                           Container(
-                            padding: EdgeInsets.only(left: 70.0, top: 10),
+                            padding: EdgeInsets.only(left: 70.0, top: 8),
                             child: Row(
                               children: [
                                 Icon(Icons.access_time,
@@ -250,7 +294,7 @@ class CaseExchangeState extends State<CaseExchange> {
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(top: 5, bottom: 5),
+                padding: EdgeInsets.only(left: 5, top: 4, right: 2, bottom: 4),
                 child: Row(
                   children: [
                     Container(
@@ -267,7 +311,7 @@ class CaseExchangeState extends State<CaseExchange> {
                           fontFamily: 'Nunita',
                           fontStyle: FontStyle.italic,
                           fontWeight: FontWeight.bold,
-                          fontSize: 17,
+                          fontSize: 16,
                           color: kButtonColor,
                         ),
                       ),
@@ -276,7 +320,7 @@ class CaseExchangeState extends State<CaseExchange> {
                 ),
               ),
               Container(
-                height: 60,
+                height: 55,
                 padding: EdgeInsets.only(left: 10, right: 10),
                 child: Expanded(
                   child: Text(
@@ -285,7 +329,10 @@ class CaseExchangeState extends State<CaseExchange> {
                     'do everything you can to protect your ...',
                     textAlign: TextAlign.justify,
                     style: TextStyle(
-                        fontFamily: 'Segoe', fontWeight: FontWeight.bold),
+                      fontFamily: 'Segoe',
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
@@ -295,18 +342,18 @@ class CaseExchangeState extends State<CaseExchange> {
                 child: Row(
                   children: [
                     Container(
-                      width: 166,
-                      height: 100,
+                      width: 168,
+                      height: 90,
                       decoration: BoxDecoration(
                           image: DecorationImage(
                               image:
                                   AssetImage('assets/icons/doctor/covid19.png'),
                               fit: BoxFit.cover)),
                     ),
-                    SizedBox(width: 5),
+                    SizedBox(width: 4),
                     Container(
-                      width: 166,
-                      height: 100,
+                      width: 168,
+                      height: 90,
                       decoration: BoxDecoration(
                           image: DecorationImage(
                               image:
@@ -325,40 +372,53 @@ class CaseExchangeState extends State<CaseExchange> {
               ),
               Container(
                 padding:
-                    EdgeInsets.only(left: 20, top: 5, right: 10, bottom: 5),
+                    EdgeInsets.only(left: 20, top: 5, right: 10, bottom: 2),
                 child: Row(
                   children: [
                     Container(
                       child: Row(
                         children: [
                           Container(
-                            width: 30,
-                            height: 30,
+                            width: 25,
+                            height: 25,
                             child: FloatingActionButton(
-                              onPressed: () {},
                               backgroundColor: Colors.white,
                               elevation: 0,
-                              child: Icon(Icons.favorite_border_rounded),
+                              child: Icon(
+                                hasBeenPressed
+                                    ? Icons.favorite_border_rounded
+                                    : Icons.favorite,
+                                color: hasBeenPressed ? null : Colors.pink,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  hasBeenPressed = !hasBeenPressed;
+                                });
+                              },
                             ),
                           ),
                           Container(
                             child: Text(
                               '3.5k',
-                              style: TextStyle(fontSize: 16),
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w700),
                             ),
                           )
                         ],
                       ),
                     ),
-                    SizedBox(width: 80),
+                    SizedBox(width: 90),
                     Container(
                       child: Row(
                         children: [
                           Container(
-                            width: 30,
-                            height: 30,
+                            width: 25,
+                            height: 25,
                             child: FloatingActionButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pushNamed(CaseDetails.tag);
+                              },
                               backgroundColor: Colors.white,
                               elevation: 0,
                               child: Icon(Icons.mode_comment_outlined),
@@ -366,17 +426,18 @@ class CaseExchangeState extends State<CaseExchange> {
                           ),
                           Container(
                             child: Text(
-                              '3.5k',
-                              style: TextStyle(fontSize: 16),
+                              '2.5k',
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w700),
                             ),
                           )
                         ],
                       ),
                     ),
-                    SizedBox(width: 80),
+                    SizedBox(width: 90),
                     Container(
-                      width: 30,
-                      height: 30,
+                      width: 25,
+                      height: 25,
                       child: FloatingActionButton(
                         onPressed: () {},
                         backgroundColor: Colors.white,
@@ -392,16 +453,32 @@ class CaseExchangeState extends State<CaseExchange> {
         ),
       ),
     );
+    final caseCardList = Expanded(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.only(top: 10, bottom: 10),
+        clipBehavior: Clip.hardEdge,
+        child: ListBody(
+          children: [
+            doctorCaseCard,
+            doctorCaseCard,
+            doctorCaseCard,
+            doctorCaseCard,
+            doctorCaseCard,
+            doctorCaseCard,
+          ],
+        ),
+      ),
+    );
     return Scaffold(
       appBar: appBar,
       backgroundColor: kBackgroundColor,
       body: Center(
-        child: ListView(
+        child: Column(
           children: [
             writeNewCase,
             categoryTotalLabel,
             verticalDivider,
-            doctorCaseCard,
+            caseCardList,
           ],
         ),
       ),
