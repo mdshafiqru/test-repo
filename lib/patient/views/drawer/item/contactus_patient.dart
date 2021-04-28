c5ec0a56fad1e8861a169e860fc25762df28c8f8
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-04-28 20:15:45 +0600

    updates

--- lib/patient/views/drawer/item/contactus_patient.dart
+++ lib/patient/views/drawer/item/contactus_patient.dart
@@ -19,7 +19,7 @@ class ContactUsPatientState extends State<ContactUsPatient> {
         child: CircleAvatar(
           backgroundColor: Colors.transparent,
           radius: 40.0,
-          child: Image.asset('assets/contactpage.png'),
+          child: Image.asset('assets/icons/patient/contactpage.png'),
         ),
       ),
     );
@@ -57,7 +57,7 @@ class ContactUsPatientState extends State<ContactUsPatient> {
             height: 250,
             padding: EdgeInsets.only(top: 70),
             child: Image.asset(
-              'assets/comingsoon.png',
+              'assets/icons/patient/comingsoon.png',
               fit: BoxFit.fill,
             ),
           ),
